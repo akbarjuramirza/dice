6a1703641b08602de4ec0dcc6d914b67b8016fb1
akbarjuramirza <a.juramirza@gmail.com>
2022-12-09 14:26:01 +0500

    I added score calculator which sums two dies scores.

--- dice/ViewController.swift
+++ dice/ViewController.swift
@@ -12,12 +12,20 @@ class ViewController: UIViewController {
     let diceImages = [#imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceScoreText: UILabel!
+    @IBOutlet weak var diceScoreIndex: UILabel!
     
-    @IBAction func rollPressed(_ sender: Any) {
-//        diceImageView1.image = diceImages[Int.random(in: 0...5)]
-//        diceImageView2.image = diceImages[Int.random(in: 0...5)]
-        diceImageView1.image = diceImages.randomElement()
-        diceImageView2.image = diceImages.randomElement()
+        @IBAction func rollPressed(_ sender: Any) {
+//        diceImageView1.image = diceImages.randomElement()
+//        diceImageView2.image = diceImages.randomElement()
+        let scoreIndex1 = Int.random(in: 0...5)
+        let scoreIndex2 = Int.random(in: 0...5)
+        diceImageView1.image = diceImages[scoreIndex1]
+        diceImageView2.image = diceImages[scoreIndex2]
+        var result = scoreIndex1+scoreIndex2+2
+        diceScoreIndex.text = String(result)
+        
+        
     }
 
     override func viewDidLoad() {
